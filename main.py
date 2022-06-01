c76ccd3977c30a036e6a6fa7d4c5db3d05a76fad
Chris Wise <wisech@vcu.edu>
2022-06-01 12:43:32 -0400

    added waitress to serve app

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from website import create_app
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    # app.run('0.0.0.0',port=server_port)
+    serve(app, host='0.0.0.0', port=8080)
