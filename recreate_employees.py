733c4818d8384cb1ad46f63fe9434ca6a918de70
chrishwise <wisech@vcu.edu>
2024-03-11 18:28:22 -0400

    recreate employees table script

--- recreate_employees.py
+++ recreate_employees.py
@@ -1,9 +1,16 @@
 from main import app
 from website import db
-from website.models import Attorney
+from website.models import Attorney, AttorneyMembership, AttorneyPublication, AttorneyAreaOfPractice, \
+    AttorneyEducation, AttorneyProfessionalLicense, AttorneyAdmission, AttorneyProfessionalActivity
 
 
 with app.app_context():
-
+    AttorneyMembership.__table__.drop(db.engine)
+    AttorneyPublication.__table__.drop(db.engine)
+    AttorneyAreaOfPractice.__table__.drop(db.engine)
+    AttorneyEducation.__table__.drop(db.engine)
+    AttorneyProfessionalActivity.__table__.drop(db.engine)
+    AttorneyProfessionalLicense.__table__.drop(db.engine)
+    AttorneyAdmission.__table__.drop(db.engine)
     Attorney.__table__.drop(db.engine)
     db.create_all()
