d25628c5ac2961320e1c4ddb19ae584305785135
chrishwise <wisech@vcu.edu>
2024-05-30 18:04:18 -0400

    fixed double click carousel bug

--- website/static/js/carousel3D.js
+++ website/static/js/carousel3D.js
@@ -17,6 +17,7 @@ class carouselItem{
 
 class carousel3d{
     carouselRoot;
+    textContainer;
     arrayOfItems;
     currentItem;
     nextItem;
@@ -24,7 +25,7 @@ class carousel3d{
     rotationIncrement;
     radius;
 
-    constructor(rootId, radius, withButtons=true) {
+    constructor(rootId, textContainerId, radius, withButtons=true) {
         this.carouselRoot = document.getElementById(rootId);
         this.radius = radius;
         this.carouselRoot.style.transform = `translateZ(-${this.radius}px)`;
@@ -44,14 +45,25 @@ class carousel3d{
             textElement.append(headingElement, descriptionElement);
             textElement.classList.add('textElement');
             newItem.setTextElement(textElement);
-            const textContainer = document.getElementById('paContent');
-            textContainer.appendChild(textElement);
+            this.textContainer = document.getElementById(textContainerId);
+            this.textContainer.appendChild(textElement);
             this.arrayOfItems.push(newItem);
+            // Rotate each item to their proper angle4
             item.style.transform = `rotateY(${rotationAngle}deg) translateZ(${radius}px)`;  // Can do this with GSAP
             rotationAngle += this.rotationIncrement;
         });
+        // Set the height of the textContainer to the height of its tallest child
+        let heightOfTallestChild = 0;
+        [...this.textContainer.children].forEach(textElement => {
+            if (textElement.offsetHeight > heightOfTallestChild) {
+                heightOfTallestChild = textElement.offsetHeight;
+            }
+        })
+        this.textContainer.style.height = heightOfTallestChild + "px";
+        // Set the current item. spinToItem sets previous and next.
         this.currentItem = this.arrayOfItems[0];
         this.spinToItem(this.currentItem);
+        // Depending on the withButtons parameter, optionally create buttons
         if (withButtons){this.createButtons();}
     }
 
@@ -71,7 +83,7 @@ class carousel3d{
     spinToItem(item){
         let angle = -item.positionAngle;
         gsap.to(this.carouselRoot, {rotationY:angle + '_short', duration:1, ease:'power4.out'});
-        gsap.to(this.currentItem.textElement, {autoAlpha:0, xPercent:-100, duration:0.5, ease: 'power4.out'});
+        gsap.to(this.currentItem.textElement, {autoAlpha:0, overwrite:'auto', xPercent:-100, duration:0.5, ease: 'power4.out'});
         gsap.fromTo(item.textElement, {autoAlpha:0, xPercent:100}, {autoAlpha:1, xPercent: 0, duration: 1, ease: 'power4.out'});
 
         this.currentItem = item;
@@ -80,10 +92,19 @@ class carousel3d{
     }
 
     updateSize(radius) {
+        // Update the carousel width by changing its radius
         this.radius = radius;
         this.carouselRoot.style.transform = `translateZ(-${this.radius}px)`;
         this.arrayOfItems.forEach(item => {
             item.element.style.transform = `rotateY(${item.positionAngle}deg) translateZ(${radius}px)`;  // Can do this with GSAP
         });
+        // Update the height of the text container
+        let heightOfTallestChild = 0;
+        [...this.textContainer.children].forEach(textElement => {
+            if (textElement.offsetHeight > heightOfTallestChild) {
+                heightOfTallestChild = textElement.offsetHeight;
+            }
+        })
+        this.textContainer.style.height = heightOfTallestChild + "px";
     }
 }
