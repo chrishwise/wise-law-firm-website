14b60f852b9e179a4aa1390fd8dca61bb2dcba25
chrishwise <wisech@vcu.edu>
2024-06-05 12:46:56 -0400

    carousel fix

--- website/static/js/carousel3D.js
+++ website/static/js/carousel3D.js
@@ -26,7 +26,6 @@ class carousel3d{
     radius;
 
     constructor(rootId, textContainerId, radius, withButtons=true) {
-        gsap.registerPlugin(Draggable);
         this.carouselRoot = document.getElementById(rootId);
         this.radius = radius;
         this.carouselRoot.style.transform = `translateZ(-${this.radius}px)`;
